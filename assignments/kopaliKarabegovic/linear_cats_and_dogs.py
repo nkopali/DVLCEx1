facc85193134f9048315236094d886ce3150897c
nkopali <nils.kopali20@live.com>
2023-04-12 16:21:32 +0200

    ok

--- assignments/kopaliKarabegovic/linear_cats_and_dogs.py
+++ assignments/kopaliKarabegovic/linear_cats_and_dogs.py
@@ -28,12 +28,91 @@ test_data = PetsDataset("../cifar-10-batches-py", Subset.TEST)
 validation_data = PetsDataset("../cifar-10-batches-py", Subset.VALIDATION)
 
 
-# Create a BatchGenerator for each dataset using the input transformation chain op.
-training_batch = BatchGenerator(training_data, len(training_data), shuffle=True, op=op)
-validation_batch = BatchGenerator(validation_data, len(validation_data), shuffle=False, op=op)
-test_batch = BatchGenerator(test_data, len(test_data), shuffle=False, op=op)
+train_len = training_data.__len__()
+test_len = test_data.__len__()
+validation_len = validation_data.__len__()
 
-# TODO: Create the LinearClassifier, loss function and optimizer. 
+num_of_samples_per_batch = 100
+train_batches = BatchGenerator(training_data,num_of_samples_per_batch, False)
+test_batches =  BatchGenerator(test_data,num_of_samples_per_batch, False)
+validation_batches = BatchGenerator(validation_data,num_of_samples_per_batch, False)
+
+
+
+
+# TODO: Create the LinearClassifier, loss function and optimizer.
+
+# model for training
+input_size = 3072 # 32*32*3
+model = LinearClassifier(input_size,training_data.num_classes())#.to(device)
+
+
+#parameters
+learning_rate = 0.0001
+epochs = 100
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+
+
+
+print("Start of the training")
+best_acc_val = 0
+for epoch in range(epochs):
+    print(f"epoch: {epoch+1}")
+    iter = train_batches.__iter__()
+    for batch_number,batch in enumerate(iter):
+        #print(f"epoch: {epoch} - batch:{batch_number}")
+        model.train()
+        data = batch.data
+        labels = batch.label
+        data = torch.from_numpy(data)
+        labels = torch.from_numpy(labels)
+        """
+        print(f"data 0  iz epoch petlje{data[0]}")
+        print(f"data leng 0  iz epoch petlje{len(data[0])}")
+        print(f"data  iz epoch petlje{data}")
+        print(f"data type iz epoch petlje{type(data)}")
+        print(f"data leng iz epoch petlje{len(data)}")"""
+        #data = data.to(device)
+        #labels = data.to(device)
+        #forward
+        scores = model(data)
+        loss = criterion(scores,labels)
+        #print(f"loss: {round(loss.item(),2)}")
+        #backward
+        optimizer.zero_grad() #grad is zero for each batch
+        loss.backward()
+        #gradient descent, updateing the weights
+        optimizer.step()
+
+        #model eval for training data
+    model.eval()
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+    best_acc = 0
+    iter = validation_batches.__iter__()
+    with torch.no_grad():
+        for batch in iter:
+            data = batch.data
+            labels = batch.label
+            data = torch.from_numpy(data)
+            labels = torch.from_numpy(labels)
+
+            scores = model(data)
+            _, predictions = scores.max(1)
+            #print(f"SCORE {scores}")
+            #print(f"type scores{type(scores)}")
+            #print(f"leng scores{(scores.shape)}")
+            #sys.exit()
+            num_correct += (predictions == labels).sum()
+            num_samples += predictions.size(0)
+            acc = float(num_correct) / float(num_samples) * 100
+            if acc > best_acc:
+                best_acc = acc
+        print(f"validation accuracy is: {round(acc, 2)}%")
+print(f"best validation acc is: {round(best_acc,2)}%")
 
 
 '''
@@ -41,51 +120,34 @@ TODO: Train a model for multiple epochs, measure the classification accuracy on 
 After training, measure the classification accuracy of the best perfroming model on the test dataset. Document your findings in the report.
 '''
 
-# Define the LinearClassifier, loss function and optimizer.
-model = LinearClassifier(input_dim=3072, num_classes=2)
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-# Define variables for best accuracy and corresponding model weights
-best_accuracy = 0.0
-best_model_weights = None
-
-# Define number of epochs and iterate through training data for each epoch
-num_epochs = 10
-for epoch in range(num_epochs):
-
-    # Train the model on training data
-    for x, y_true in training_batch:
-        optimizer.zero_grad()
-        y_pred = model(x)
-        loss = loss_fn(y_pred, y_true)
-        loss.backward()
-        optimizer.step()
-
-    # Evaluate the model on validation data and measure accuracy
-    accuracy = Accuracy()
+def check_accuracy(loader,model):
+    num_correct = 0
+    num_samples = 0
+    best_acc = 0
+    model.eval()
+    iter = loader.__iter__()
     with torch.no_grad():
-        for x, y_true in validation_batch:
-            y_pred = model(x)
-            y_pred = torch.argmax(y_pred, dim=1)
-            accuracy.update_state(y_true.numpy(), y_pred.numpy())
+        for batch in iter:
+            data = batch.data
+            labels = batch.label
+            data = torch.from_numpy(data)
+            labels = torch.from_numpy(labels)
 
-    # Check if current accuracy is better than previous best accuracy and save model weights if it is
-    if accuracy.get() > best_accuracy:
-        best_accuracy = accuracy.get()
-        best_model_weights = model.state_dict()
+            scores = model(data)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == labels).sum()
+            num_samples += predictions.size(0)
+            acc = float(num_correct)/float(num_samples)*100
+            if acc > best_acc:
+                best_acc = acc
+            print(f"Accuracy is: {round(acc,2)}%")
+    print(f"best test acc is: {round(best_acc,2)}%")
+"""
+print("Train accuracy: ")
+check_accuracy(train_batches,model)
 
-    # Print current epoch and validation accuracy
-    print(f"Epoch {epoch+1}, Validation Accuracy: {accuracy.get()}")
+print("validation accuracy: ")
+check_accuracy(validation_batches,model)"""
 
-# Load best performing model weights and evaluate on test data
-model.load_state_dict(best_model_weights)
-accuracy = Accuracy()
-with torch.no_grad():
-    for x, y_true in test_batch:
-        y_pred = model(x)
-        y_pred = torch.argmax(y_pred, dim=1)
-        accuracy.update_state(y_true.numpy(), y_pred.numpy())
-
-# Print test accuracy
-print(f"Test Accuracy: {accuracy.get()}")
+print("Test accuracy is: ")
+check_accuracy(test_batches,model)
